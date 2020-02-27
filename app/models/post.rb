c1697da35e086f7db1ddd95663c5468286f6cb29
github username <github email address>
2020-02-26 17:10:55 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"],
+    message: "Please enter a category." }
 end
